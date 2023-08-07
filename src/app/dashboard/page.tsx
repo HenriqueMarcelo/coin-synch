9c1345d8d876886c0698133deda953c122a9f0c9
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-07 12:21:06 -0300

    feat: setup grid

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,30 @@
 import './page.scss'
 
 export default function Dashboard() {
-  return <h1 className="Dashboard_container">Olá mundo</h1>
+  return (
+    <h1 className="Dashboard_container container ">
+      <div className="grid grid-cols-12 gap-8">
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>01</div>
+        <div>09</div>
+        <div>10</div>
+        <div>09</div>
+        <div>09</div>
+        <div>09</div>
+        <div>09</div>
+        <div>09</div>
+        <div>09</div>
+        <div>09</div>
+        <div>09</div>
+        <div>19</div>
+        <div>20</div>
+      </div>
+    </h1>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '632px',
+        md: '704px', // alterado
+        lg: '1016px',
+        xl: '1248px', // alterado
+        // '2xl': '1496px', //removido
+      },
+    },
     extend: {},
   },
   plugins: [],
