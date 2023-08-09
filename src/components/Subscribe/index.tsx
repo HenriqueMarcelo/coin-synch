2aa4e2690ceb5745513468a0ef71377cf78b82b5
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-09 14:21:08 -0300

    feat: save newsletters into db

--- src/components/Subscribe/index.tsx
+++ src/components/Subscribe/index.tsx
@@ -1,6 +1,36 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
 import './styles.scss'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { apiJson } from '@/lib/axios'
 
 export function Subscribe() {
+  const subscribeSchema = z.object({
+    email: z.string().email(),
+  })
+
+  type Subscribe = z.infer<typeof subscribeSchema>
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<Subscribe>({
+    resolver: zodResolver(subscribeSchema),
+  })
+
+  async function onSubmit({ email }: Subscribe) {
+    await apiJson.post('newsletter', {
+      email,
+    })
+    reset()
+
+    // todo criar um toast de feedback
+  }
+
   return (
     <section className="subscribe__full">
       <div className="subscribe__container container">
@@ -13,12 +43,22 @@ export function Subscribe() {
             porttitor
           </p>
         </div>
-        <form action="" className="subscribe__form">
+        <form className="subscribe__form" onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="" className="subscribe__label">
             Email
-            <input placeholder="Email" className="subscribe__input" />
+            <input
+              placeholder="Email"
+              className={`subscribe__input ${
+                errors.email && 'subscribe__input--error'
+              }`}
+              type="email"
+              disabled={isSubmitting}
+              {...register('email')}
+            />
           </label>
-          <button className="subscribe__button">Subscribe</button>
+          <button disabled={isSubmitting} className="subscribe__button">
+            Subscribe
+          </button>
         </form>
       </div>
     </section>
