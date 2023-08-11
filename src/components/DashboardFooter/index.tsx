4f167ff0cc78e68724d4fbda7bd962476b8865dd
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-11 13:15:30 -0300

    feat: create style for dashboard table in tablet

--- src/components/DashboardFooter/index.tsx
+++ src/components/DashboardFooter/index.tsx
@@ -1,9 +1,12 @@
 import './styles.scss'
+import HeaderLogoSvg from '../../assets/header-logo.svg'
+import Image from 'next/image'
 
 export function DashboardFooter() {
   return (
     <footer className="dashboard-footer__container">
-      Copyright © 2022 - All rights reserved
+      <span>Copyright © 2022 - All rights reserved</span>
+      <Image className="dashboard-footer__image" src={HeaderLogoSvg} alt="" />
     </footer>
   )
 }
